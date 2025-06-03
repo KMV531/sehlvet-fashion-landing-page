7e0e616f6e2380a44be9f95f47d01d3277ec1d05
KMV531 <koladjamomovinnybrayan3@gmail.com>
2025-06-03 08:56:10 +0200

    Set up completed

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="heading">This uses Wilkysta</h1>
+      <h1 className="sub-heading">Elegant Design with Playfair Display</h1>
+    </div>
+  );
+};
+
+export default HomePage;
